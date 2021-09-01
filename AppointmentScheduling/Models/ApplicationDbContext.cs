28cbfe77f597c6a22112806f1af41a2cfa9bab1f
RigvedBhavsar <rigvedbhavsar@gmail.com>
2021-09-01 23:40:26 +0530

    Design Signin and Signup Screen

---
+++ AppointmentScheduling/Models/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ) : base(options)
+        {
+
+        }
+    }
+}
+
+
+//Inheriting IdentityDbContext cause it gives the features of Login/Authentication
+
+/*
+                        How To Add Migration
+    0. Add Connection String into appsetting.json file
+    1.Inside Models Folder create ApplcicationDbContext and Add DbSet
+    2.Install tools for Microsoft Entity Core from nuget
+    3.open nuget Console and fire Add-migration AddIdentityTable
+        (This will create table for Identity / Users login )
+    4.fire Update-Databse to push tables to Db
+*/
---
+++ AppointmentScheduling/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} Must Be At Least {2} Character Long.", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name="Confirm Password")]
+        [Compare("Password", ErrorMessage ="The Password And Confirm Password Does Not Match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name="Role Name")]
+        public string RoleName { get; set; }
+
+    }
+}
